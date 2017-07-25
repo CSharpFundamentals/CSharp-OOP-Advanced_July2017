24bc41e341ebc0649412c9ffe88ba42899eb605a
CSharpFundamentals <v.dimitrov@softuni.bg>
2017-07-25 23:28:34 +0300

    Iterators and Comparators - Lab

---
+++ 04.Iterators and Comparators - Lab/3.Comparable Book/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Book : IComparable<Book>
+{
+    public Book(string title, int year, params string[] authors)
+    {
+        this.Title = title;
+        this.Year = year;
+        this.Authors = new List<string>(authors);
+    }
+
+    public string Title { get; set; }
+    public int Year { get; set; }
+    public IReadOnlyList<string> Authors { get; set; }
+
+    public int CompareTo(Book otherBook)
+    {
+        int result = this.Year.CompareTo(otherBook.Year);
+        if (result == 0)
+        {
+            result = this.Title.CompareTo(otherBook.Title);
+        }
+
+        return result;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Title} - {this.Year}";
+    }
+}
