43c6f332af26f04b3fc23ac1fb53449b6fd843fa
CSharpFundamentals <v.dimitrov@softuni.bg>
2017-07-25 23:06:07 +0300

    Iterators and Comparators - Lab

---
+++ 2.Library Iterator/Library.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Library : IEnumerable<Book>
+{
+    private readonly List<Book> books;
+
+    public Library(params Book[] books)
+    {
+        this.books = new List<Book>(books);
+    }
+
+    public IEnumerator<Book> GetEnumerator()
+    {
+        return new LibraryIterator(this.books);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+
+    private class LibraryIterator : IEnumerator<Book>
+    {
+        private readonly List<Book> books;
+        private int currentIndex;
+
+        public LibraryIterator(IEnumerable<Book> books)
+        {
+            this.Reset();
+            this.books = new List<Book>(books);
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            return ++currentIndex < this.books.Count;
+        }
+
+        public void Reset()
+        {
+            this.currentIndex = -1;
+        }
+
+        public Book Current
+        {
+            get => this.books[currentIndex];
+        }
+
+        object IEnumerator.Current
+        {
+            get { return this.Current; }
+        }
+    }
+}
