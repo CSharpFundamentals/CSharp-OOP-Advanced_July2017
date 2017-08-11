623facec264c2087b7098f219c841f93d22a9101
CSharpFundamentals <v.dimitrov@softuni.bg>
2017-08-11 22:19:01 +0300

    Add files via upload

---
+++ 12.Events - Exercise/DependencyInversion/PrimitiveCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+class PrimitiveCalculator
+{
+    private IStrategy strategy;
+    private IDictionary<char, IStrategy> strategies;
+
+    public PrimitiveCalculator(IStrategy strategy, IDictionary<char, IStrategy> strategies)
+    {
+        this.strategy = strategy;
+        this.strategies = strategies;
+    }
+
+    public void ChangeStrategy(char @operator)
+    {
+        this.strategy = this.strategies[@operator];
+    }
+
+    public int performCalculation(int firstOperand, int secondOperand)
+    {
+        return this.strategy.Calculate(firstOperand, secondOperand);
+    }
+}
